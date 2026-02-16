b0d313d7cb42920821c656924c48c4d9fcd0154b
puneetayam <puneetayam23@gmail.com>
2026-02-16 18:36:32 +0530

    done insta project part-1

--- Backend/01_project-instagram-clone/src/routes/auth.routes.js
+++ Backend/01_project-instagram-clone/src/routes/auth.routes.js
@@ -3,8 +3,14 @@ const authController = require("../controllers/auth.controller");
 
 const authRouter = express.Router();
 
+/**
+ * POST /api/auth/register
+ */
 authRouter.post("/register", authController.registerController);
 
+/**
+ * POST /api/auth/login
+ */
 authRouter.post("/login", authController.loginController);
 
 module.exports = authRouter;
